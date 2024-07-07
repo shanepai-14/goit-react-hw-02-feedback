9e5563cd57199e78ade2fdfcfbc735451c6b5516
Shane <96838519+shanepai-14@users.noreply.github.com>
2024-07-07 20:41:34 +0800

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -9,7 +9,7 @@ const Header = () => {
     const location = useLocation();
 
     useEffect(() => {
-        if (location.pathname === '/') {
+        if (location.pathname === '/goit-react-hw-02-feedback') {
             setValue(0);
         } if (location.pathname === '/feedback') {
             setValue(1);
